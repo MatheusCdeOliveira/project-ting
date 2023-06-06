9b40023c5dc8acd3bc96539589ea948cfbc02f8c
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2023-06-06 18:09:36 -0300

    feat: process file method implemented

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file
+        }
+    for e in instance.queue:
+        if e["nome_do_arquivo"] == data["nome_do_arquivo"]:
+            return None
+    instance.enqueue(data)
+    print(data, file=sys.stdout)
 
 
 def remove(instance):
