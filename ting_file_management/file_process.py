2222db8b12eee9be04d445c9e3a93264a66daa07
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2023-06-06 18:22:02 -0300

    feat: method to remove from queue implemented

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -17,7 +17,14 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if not instance.queue:
+        print('Não há elementos', file=sys.stdout)
+    else:
+        removed = instance.dequeue()
+        print(f"""
+              Arquivo {removed["nome_do_arquivo"]} removido com sucesso
+              """, file=sys.stdout)
+
 
 
 def file_metadata(instance, position):
