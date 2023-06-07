c38eab237f55270fdb635bc5856fa52d06d25640
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2023-06-07 18:29:56 -0300

    feat: method that search by word implemented)

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,18 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for dicts in instance.queue:
+        data = {
+            "palavra": word,
+            "arquivo": dicts["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        for i, s in enumerate(dicts["linhas_do_arquivo"]):
+            if word.lower() in s.lower():
+                data["ocorrencias"].append({"linha": i + 1})
+        if len(data["ocorrencias"]):
+            result.append(data)
+
+    return result
 
 
 def search_by_word(word, instance):
